62c125270b8b1ca90560e350b7caf9f7c3ec7afd
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-16 08:33:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 # Write your code here.
 def line(katzLine)
   if katzLine.size == 0 
+    puts "The line is currently empty."
     return "The line is currently empty."
   else
     range = 0..katzLine.length-1
@@ -20,3 +21,4 @@ def now_serving(katzLine)
   
 end
 
+katzLine
