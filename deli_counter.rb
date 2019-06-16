6896de1d0ca30be530013fcc7108504197b765fc
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-16 08:27:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,11 @@
 def line(katzLine)
   if katzLine.length == 0 
     puts "The line is currently empty."
+  else
+    range = 0..katzLine.length-1
+    for person in range
+      puts 
   end
-  
 end
 
 def take_a_number(katzLine, name)
